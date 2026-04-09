4d0ca9d65a4b1248fe5b20613494afd2922acdfb
WOOJAE JO <whgjsgh11@gmail.com>
2026-04-09 14:13:45 +0900

    init

---
+++ src/test/java/dev/example/jenkins/first_jenkins_sample/FirstJenkinsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.example.jenkins.first_jenkins_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJenkinsSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
