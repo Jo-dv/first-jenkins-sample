598de274c39b00b11536946f09d45e4f2c269ab4
WOOJAE JO <whgjsgh11@gmail.com>
2026-04-10 12:37:52 +0900

    test

--- src/main/java/com/example/first_jenkins_sample/HelloController.java
+++ src/main/java/com/example/first_jenkins_sample/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        System.out.println("test1");
+        System.out.println("test2");
         return "Hello, Jenkins CI/CD!";
     }
 }
